45c0cf69edde6acdd7003d1ddd4563373363a87b
Daniel H. Kikuti <zizaounesprc@gmail.com>
2018-06-29 22:37:29 -0300

    Add files via upload

--- src/jsoupdemo/JsoupDemo.java
+++ src/jsoupdemo/JsoupDemo.java
@@ -1,45 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jsoupdemo;
 
-import java.io.*;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.*;
-import org.jsoup.select.*;
-
-/**
- *
- * @author raduke
- */
 public class JsoupDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-         String url;
-        url = "https://lista.mercadolivre.com.br/smartphone";
+        System.out.println("------------------ MERCADO LIVRE ------------------");
+        MercadoLivre mercliv = new MercadoLivre();
+        mercliv.pesquisar();
         
-        try
-        {
-            Document doc = Jsoup.connect(url).get();
-            Elements produtos = doc.getElementsByClass("results-item");
-            for(Element t : produtos)
-            {
-                String texto = t.getElementsByClass("main-title").first().text();
-                System.out.println(texto);
-                String preco = t.getElementsByClass("price__fraction").first().text();
-                String moeda = t.getElementsByClass("price__symbol").first().text();
-                System.out.println(moeda+preco);
-            }
-        }
-        catch(IOException err)
-        {
-            System.out.print(err.getMessage());
-        }
+        System.out.println("------------------- AMERICANAS --------------------");
+        Americanas amer = new Americanas();
+        amer.pesquisar();
+        
+        System.out.println("------------------ MAGAZINELUIZA ------------------");
+        MagazineLuiza magalu = new MagazineLuiza();
+        magalu.pesquisar();
     }
     
 }
