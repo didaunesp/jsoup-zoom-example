ba79efbddaa1f364d6b28b634357ef625936fde9
zizao <zizaounesprc@gmail.com>
2018-07-05 02:06:05 -0300

    tirando comentários de autor e de package

--- src/br/unesp/rc/jsoupDemo/dao/ProdutoDAOImpl.java
+++ src/br/unesp/rc/jsoupDemo/dao/ProdutoDAOImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.unesp.rc.jsoupDemo.dao;
 
 import br.unesp.rc.jsoupDemo.model.Produto;
@@ -12,10 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- *
- * @author raduke
- */
 public class ProdutoDAOImpl implements ProdutoDAO{
     public boolean salvar(Produto produto){
         Connection con = null;
