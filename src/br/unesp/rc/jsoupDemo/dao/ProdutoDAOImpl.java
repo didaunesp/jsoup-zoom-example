5731aaf398278032831992ed90543d9ac971faca
Unknown <gferreira525@gmail.com>
2018-07-05 01:47:59 -0300

    salvando preco do produto

--- src/br/unesp/rc/jsoupDemo/dao/ProdutoDAOImpl.java
+++ src/br/unesp/rc/jsoupDemo/dao/ProdutoDAOImpl.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 /**
  *
@@ -22,18 +23,35 @@ public class ProdutoDAOImpl implements ProdutoDAO{
         PreparedStatement pstm = null;
         ResultSet res = null;
         boolean b = false;
+        long idProduto = -1;
         
         con = FabricaConexao.getConexao();
         if (con != null) {
             try {
                 con.setAutoCommit(false);
                 
-                // edao.salvar(con, e, idCliente); - considerar exemplo pra salvar preco
-                
-                pstm = con.prepareStatement(INSERT_PRODUTO);
+                //executa inserção do produto
+                pstm = con.prepareStatement(INSERT_PRODUTO,
+                        PreparedStatement.RETURN_GENERATED_KEYS);
                 pstm.setString(1, produto.getNome());
                 pstm.executeUpdate();
-
+                
+                res = pstm.getGeneratedKeys();
+                while (res.next()) {
+                    idProduto = res.getLong(1);
+                }
+                
+                pstm = con.prepareStatement(INSERT_PRECO);
+                pstm.setLong(1, idProduto);
+                pstm.setString(2, produto.getPreco());
+                
+                //OBTEM DATA ATUAL TODO: ENCAPSULAR EM UM HELPER
+                //DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = new Date();
+                //String hoje = dateFormat.format(date);
+                
+                pstm.setDate(3, (java.sql.Date) date);
+                
                 con.commit();
                 b = true;
             } catch (SQLException ex) {
