bd365f4c7196cc79901c037ea202fbf8fc46ee6e
zizao <zizaounesprc@gmail.com>
2018-07-05 12:26:11 -0300

    Tirando comentario

--- Projeto Jsoup/netbeans/JsoupWeb/src/java/br/unesp/rc/jsoupDemo/dao/ProdutoDAOImpl.java
+++ Projeto Jsoup/netbeans/JsoupWeb/src/java/br/unesp/rc/jsoupDemo/dao/ProdutoDAOImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.unesp.rc.jsoupDemo.dao;
 
 import br.unesp.rc.jsoupDemo.model.Produto;
@@ -13,10 +8,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
-/**
- *
- * @author raduke
- */
 public class ProdutoDAOImpl implements ProdutoDAO{
     public boolean salvar(Produto produto){
         Connection con = null;
